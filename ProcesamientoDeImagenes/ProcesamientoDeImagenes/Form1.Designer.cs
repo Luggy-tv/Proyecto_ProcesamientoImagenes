2c6adb4c8fd2a028e947e7e78e6b1b40cdc5d5ae
Luis Alfonso Martinez Martinez <luisalfonsomartinezmartinez@outlook.es>
2023-07-03 20:38:52 -0600

    Creacion de proyecto y ventana basica con botones

---
+++ ProcesamientoDeImagenes/ProcesamientoDeImagenes/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ProcesamientoDeImagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelMenu = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panelMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(17, 0, 158);
+            panelMenu.Controls.Add(button3);
+            panelMenu.Controls.Add(button2);
+            panelMenu.Controls.Add(button1);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(190, 591);
+            panelMenu.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(73, 66, 228);
+            button3.BackgroundImageLayout = ImageLayout.Center;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = Color.Gainsboro;
+            button3.Image = Properties.Resources.Camera_100x100;
+            button3.Location = new Point(5, 400);
+            button3.Name = "button3";
+            button3.Size = new Size(180, 190);
+            button3.TabIndex = 2;
+            button3.Text = "Abrir Camara";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(73, 66, 228);
+            button2.BackgroundImageLayout = ImageLayout.Center;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.Gainsboro;
+            button2.Image = Properties.Resources.Video_100x100;
+            button2.Location = new Point(5, 200);
+            button2.Name = "button2";
+            button2.Size = new Size(180, 190);
+            button2.TabIndex = 1;
+            button2.Text = "Seleccionar Video";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(73, 66, 228);
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Gainsboro;
+            button1.Image = Properties.Resources.Image_100x100;
+            button1.Location = new Point(5, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(180, 190);
+            button1.TabIndex = 0;
+            button1.Text = "Seleccionar Foto";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(134, 150, 254);
+            ClientSize = new Size(984, 591);
+            Controls.Add(panelMenu);
+            Name = "Form1";
+            Text = "Form1";
+            panelMenu.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelMenu;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+    }
+}
---
+++ ProcesamientoDeImagenes/ProcesamientoDeImagenes/Form1.cs
@@ -0,0 +1,11 @@
+namespace ProcesamientoDeImagenes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
