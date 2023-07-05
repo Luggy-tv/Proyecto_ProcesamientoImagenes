5f5dd70c092b21cc94816ecda11d96dc089d7c10
Luis Alfonso Martinez Martinez <luisalfonsomartinezmartinez@outlook.es>
2023-07-04 20:07:04 -0600

    camara basica y diseño

--- ProcesamientoDeImagenes/ProcesamientoDeImagenes/Form1.Designer.cs
+++ ProcesamientoDeImagenes/ProcesamientoDeImagenes/Form1.Designer.cs
@@ -28,11 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panelMenu = new Panel();
             button3 = new Button();
             button2 = new Button();
             button1 = new Button();
+            pictureBox1 = new PictureBox();
+            comboBox1 = new ComboBox();
+            button4 = new Button();
+            label1 = new Label();
+            button5 = new Button();
+            button6 = new Button();
             panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panelMenu
@@ -62,6 +70,7 @@
             button3.TabIndex = 2;
             button3.Text = "Abrir Camara";
             button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
             // button2
             // 
@@ -76,8 +85,9 @@
             button2.Name = "button2";
             button2.Size = new Size(180, 190);
             button2.TabIndex = 1;
-            button2.Text = "Seleccionar Video";
+            button2.Text = "Seleccionar   Video";
             button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // button1
             // 
@@ -92,8 +102,79 @@
             button1.Name = "button1";
             button1.Size = new Size(180, 190);
             button1.TabIndex = 0;
-            button1.Text = "Seleccionar Foto";
+            button1.Text = "Seleccionar Imagen";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(214, 27);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(475, 294);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(714, 53);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(258, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(73, 66, 228);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.ForeColor = Color.Gainsboro;
+            button4.Location = new Point(714, 124);
+            button4.Name = "button4";
+            button4.Size = new Size(258, 66);
+            button4.TabIndex = 3;
+            button4.Text = "Aplicar Filtro";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(714, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(233, 30);
+            label1.TabIndex = 4;
+            label1.Text = "Selecciona una opcion:";
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(17, 0, 158);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.ForeColor = Color.Gainsboro;
+            button5.Location = new Point(714, 255);
+            button5.Name = "button5";
+            button5.Size = new Size(258, 66);
+            button5.TabIndex = 5;
+            button5.Text = "Guardar Imagen";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(196, 186, 255);
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.Location = new Point(714, 200);
+            button6.Name = "button6";
+            button6.Size = new Size(258, 40);
+            button6.TabIndex = 6;
+            button6.Text = "Quitar Filtro";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
             // 
             // Form1
             // 
@@ -101,11 +182,22 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(134, 150, 254);
             ClientSize = new Size(984, 591);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(comboBox1);
+            Controls.Add(pictureBox1);
             Controls.Add(panelMenu);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
-            Text = "Form1";
+            Text = "Procesamiento de Imagenes";
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
             panelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -114,5 +206,11 @@
         private Button button1;
         private Button button3;
         private Button button2;
+        private PictureBox pictureBox1;
+        private ComboBox comboBox1;
+        private Button button4;
+        private Label label1;
+        private Button button5;
+        private Button button6;
     }
 }
--- ProcesamientoDeImagenes/ProcesamientoDeImagenes/Form1.cs
+++ ProcesamientoDeImagenes/ProcesamientoDeImagenes/Form1.cs
@@ -1,11 +1,115 @@
+using AForge.Video.DirectShow;
+using AForge.Video;
+
 namespace ProcesamientoDeImagenes
 {
     public partial class Form1 : Form
     {
+
+        private bool hayDispositivos = false;
+        private FilterInfoCollection misDispositivos;
+        private VideoCaptureDevice miWebCam = null;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cargarDispositivios()
+        {
+            misDispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            
+            if (misDispositivos.Count > 0)
+            {
+                
+                comboBox1.Items.Clear();
+
+                hayDispositivos = true;
+                for (int i = 0; i < misDispositivos.Count; i++)
+                {
+                    comboBox1.Items.Add(misDispositivos[i].Name.ToString());
+                }
+
+                comboBox1.Text = misDispositivos[0].Name.ToString();
+
+                //Cambiar esto a otro boton ya que es de la camara
+                #region Cosas de Camara que hay que quitar de aqui
+                cerrarWebCam();
+                int j = comboBox1.SelectedIndex;
+                string nombreVideo = misDispositivos[j].MonikerString;
+                miWebCam = new VideoCaptureDevice(nombreVideo);
+                miWebCam.NewFrame += new NewFrameEventHandler(actualizarCamara);
+                miWebCam.Start();
+
+                #endregion
+            }
+            else
+            {
+                hayDispositivos = false;
+            }
+        }
+        public void cerrarWebCam()
+        {
+            if (miWebCam != null && miWebCam.IsRunning)
+            {
+                miWebCam.SignalToStop();
+                miWebCam = null;
+            }
+        }
+
+        //Boton De Aplicar Filtro
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Boton de Seleccionar imagen
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cerrarWebCam();
+
+        }
+
+        //Boton de abrir camara
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cerrarWebCam();
+            cargarDispositivios();
+        }
+        //Metodo para actualizar PictureBox para camara 
+        private void actualizarCamara(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap imagen = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = imagen;
+        }
+
+        //Boton de Seleccionar video
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cerrarWebCam();
+
+        }
+
+        //Boton de quitar filtro
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Boton de Guardar Imagen
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            cerrarWebCam();
+        }
     }
 }
